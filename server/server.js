3538ba2fc18571237c67bdc43c916e1344f0f274
AAyinkx <ayinkafinowi@gmail.com>
2024-09-25 10:39:15 +0100

    Changes made to database through seed file

--- server/server.js
+++ server/server.js
@@ -9,33 +9,31 @@ app.use(express.json());
 app.use(cors());
 
 dotenv.config();
-const db = new pg.Pool({
-    connectionString: process.env.DATABASE_URL
-})
+export const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 const PORT = 8080;
-app.listen(PORT, function(){
+app.listen(PORT, function () {
   console.log(`server is running in port ${PORT}`);
 });
 
-app.get("/",function (req, res) {
+app.get("/", function (req, res) {
   res.json({ message: "this is the root route" });
 });
 
-app.post("/add",async function(req,res){
-
- const queryResponse = await db.query('SELECT * FROM sign_in WHERE user_email= $1 AND user_password = $2',[req.body.formValues.your_name ,req.body.formValues.your_pass] );
+app.post("/add", async function (req, res) {
+  const queryResponse = await db.query(
+    "SELECT * FROM sign_in WHERE user_email= $1 AND user_password = $2",
+    [req.body.formValues.your_name, req.body.formValues.your_pass]
+  );
 
   console.log(queryResponse.rowCount);
   if (queryResponse.rowCount === 1) {
     console.log("welcome");
-    res.json({massage:"Welcome User"});
+    res.json({ massage: "Welcome User" });
   } else {
     console.log("error");
-    res.json({massage:"Invalid Login"});
+    res.json({ massage: "Invalid Login" });
   }
-
-
-
-  
 });
