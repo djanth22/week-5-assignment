4b5bda9814313beb5266bd0de37873e870b6e019
David Anthony <davywavy22@googlemail.com>
2024-09-25 10:08:45 +0100

    fotgot to save, should be up to date completely

--- client/app.js
+++ client/app.js
@@ -1,35 +1,3 @@
-const loginform = document.querySelector("#login-form");
-async function handleSubmit(event) {
-  event.preventDefault();
-
-  const formData = new FormData(loginform);
-  const formValues = Object.fromEntries(formData);
-  console.table(formValues);
-
-  const msgRes = await fetch("http://localhost:8080/add", {
-    method: "POST", // This is where we set the POST HTTP verb
-    headers: {
-      "Content-Type": "application/json", // This tells the server we're sending stringified JSON data
-    },
-    body: JSON.stringify({ formValues }),
-  });
-  const msg = await msgRes.json();
-
-  const msgCheker = document.querySelector("#login_massage");
-
-  if (msgCheker === null) {
-    const h1 = document.createElement("h1");
-    h1.id = "login_massage";
-    h1.textContent = msg.massage;
-    form.appendChild(h1);
-  } else {
-    msgCheker.textContent = msg.massage;
-  }
-
-  form.reset();
-}
-form.addEventListener("submit", handleSubmit);
-
 const form = document.getElementById(`form-container`);
 const toggle = document.getElementById(`hide-show`);
 let display = true;
